5656ed5eafbb52a72437361121723b6996d1d42a
MichaelBonsall <fanvsant2@gmail.com>
2022-06-22 21:53:49 -0700

    basics

--- test.py
+++ test.py
@@ -1,15 +1,12 @@
 from PIL import Image
-#Not sure if I want to use numpy to make matrix or natively do it. numpy allows various functions but i dont think we'll need em
-#no import required
-#matrix = [[0]*5 for i in range(5)]
-#import required
+import numpy
 
-#import numpy
-#numpy.zeros((5, 5))
+a = numpy.zeros((16, 16))
+a[3][4] = 4
+print(a)
 
-#might want to add a way to "animate" the drawing of the pixel. Would be super slow but it wil look pretty
 def newImage():
-    img = Image.new('RGB', (48, 48))
+    img = Image.new('RGB', (48,48 ))
     img.putpixel((10,10), (155,155,55))
     img.putpixel((11,10), (155,155,55))
     img.putpixel((10,11), (155,155,55))
