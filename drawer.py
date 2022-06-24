0faef52bfda505ac0db6ca33902acdacd32d969a
MichaelBonsall <fanvsant2@gmail.com>
2022-06-23 19:44:10 -0700

    seems good

--- drawer.py
+++ drawer.py
@@ -4,9 +4,17 @@ from PIL import Image
 import numpy
 import tiles 
 
-matrix = numpy.zero
+matrix = numpy.zeros((16,16))
 img = Image.new('RGB', (64, 64 ))
 img.save('wtf.png')
 tiles.water(0,0)
 
 img.show
+
+
+
+
+
+
+
+
