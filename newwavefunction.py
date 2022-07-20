403fb89b49fefabe465fae576c65caaee40488ee
simone-lofaso <99053610+simone-lofaso@users.noreply.github.com>
2022-07-19 22:46:16 -0700

    added tiles for new map

--- newwavefunction.py
+++ newwavefunction.py
@@ -2,7 +2,7 @@ import numpy
 from PIL import Image
 import newtiles
 
-map = numpy.zeros((16,16))
+map = numpy.zeros((24,24))
 
 #map 3d array used for tracking what tiles can be put at that given location. Everything starts as true
 
@@ -12,6 +12,7 @@ map = numpy.zeros((16,16))
 #4 is rock
 #5 is flower
 
+#expand to 24x24
 mapBool = numpy.array([[[True, True, True, True, True], [True, True, True, True, True],[True, True, True, True, True],[True, True, True, True, True],[True, True, True, True, True], [True, True, True, True, True], [True, True, True, True, True], [True, True, True, True, True], [True, True, True, True, True], [True, True, True, True, True], [True, True, True, True, True], [True, True, True, True, True], [True, True, True, True, True], [True, True, True, True, True], [True, True, True, True, True], [True, True, True, True, True]],
                        [[True, True, True, True, True], [True, True, True, True, True],[True, True, True, True, True],[True, True, True, True, True],[True, True, True, True, True], [True, True, True, True, True], [True, True, True, True, True], [True, True, True, True, True], [True, True, True, True, True], [True, True, True, True, True], [True, True, True, True, True], [True, True, True, True, True], [True, True, True, True, True], [True, True, True, True, True], [True, True, True, True, True], [True, True, True, True, True]],
                        [[True, True, True, True, True], [True, True, True, True, True],[True, True, True, True, True],[True, True, True, True, True],[True, True, True, True, True], [True, True, True, True, True], [True, True, True, True, True], [True, True, True, True, True], [True, True, True, True, True], [True, True, True, True, True], [True, True, True, True, True], [True, True, True, True, True], [True, True, True, True, True], [True, True, True, True, True], [True, True, True, True, True], [True, True, True, True, True]],
@@ -33,9 +34,10 @@ mapBool = numpy.array([[[True, True, True, True, True], [True, True, True, True,
 
                
 img = Image.new('RGB', (96, 96))
-newtiles.flower(0, 0)
-newtiles.flower(1, 0)
-newtiles.flower(0, 1)
-newtiles.flower(1, 1)
+newtiles.flower(0,0)
+newtiles.rock(0, 1)
+newtiles.land(0, 2)
+newtiles.coast(0, 3)
+newtiles.water(0, 4)
 #img.save('output2.png')
 #img.show
