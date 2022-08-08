9c8dbcf9689009b457fea5545353ac25aae73bde
MichaelBonsall <fanvsant2@gmail.com>
2022-08-07 22:02:14 -0700

    video

--- wavefunction.py
+++ wavefunction.py
@@ -48,10 +48,10 @@ img = Image.new('RGB', (144, 144))
 
 img.save('output.png')
 img.show
-time.sleep(2)
+#time.sleep(2)
 while True:
     FLOWER_CHANCE, ROCK_CHANCE, WATER_CHANCE, COAST_CHANCE, LAND_CHANCE, xIndex, yIndex = tiles.getLowestEntropy()
     tiles.chooseTile(xIndex, yIndex, FLOWER_CHANCE, ROCK_CHANCE, WATER_CHANCE, COAST_CHANCE, LAND_CHANCE)
     img.show
-    time.sleep(.2)
+    #time.sleep(.2)
 
